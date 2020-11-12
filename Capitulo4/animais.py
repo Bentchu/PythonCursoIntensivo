237e664a3ab67bcaaff2b7ca75501959e1c366f7
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-11-12 15:18:19 -0300

    Add files via upload

---
+++ Capitulo4/animais.py
@@ -0,0 +1,6 @@
+#Curso Intensivo de Python - Eric Matthes - 4.2 pg.97
+animais=['Canário', 'Sabiá', 'Bem-te-vi', 'Tucano', 'Coleirinha']
+for animal in animais:
+	print("Um " + animal.title() + " seria um ótimo animal de estimação!\n")
+	
+print("Qualquer pássaro seria um ótimo animal de estimação!")
---
+++ Capitulo4/pizzas.py
@@ -0,0 +1,6 @@
+#Curso Intensivo de Python - Eric Matthes - 4.1 pg.97
+pizzas=['Margherita','Calabreza','Cinco Queijos','Atum','Lombo']
+for pizza in pizzas:
+		print("Gosto de pizza de " + pizza.title() + "!\n")
+		
+print("Eu realmente amo pizza!")
