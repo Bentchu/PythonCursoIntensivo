41640a47a2e004f0794d64495035509072f5202e
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-11-25 14:39:25 -0300

    Delete pizzas.py

--- Capitulo4/pizzas.py
+++
@@ -1,6 +0,0 @@
-#Curso Intensivo de Python - Eric Matthes - 4.01 pg.97
-pizzas=['Margherita','Calabreza','Cinco Queijos','Atum','Lombo']
-for pizza in pizzas:
-		print("Gosto de pizza de " + pizza.title() + "!\n")
-	
-print("Eu realmente amo pizza!")
