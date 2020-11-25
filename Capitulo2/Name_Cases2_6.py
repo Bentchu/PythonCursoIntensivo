d300fb93ed9ca7d00ace1c40d7f736cdc9a7e1a9
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-11-25 14:33:55 -0300

    Delete Name_Cases2_6.py

--- Capitulo2/Name_Cases2_6.py
+++
@@ -1,3 +0,0 @@
-famous_person = "Anitta"
-message = ', certa vez, cantou: "Então sai, sai, sai da minha frente Hoje vou dar trabalho numa onda diferente"...fantástico!'
-print famous_person + message	
