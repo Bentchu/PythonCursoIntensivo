fa7f7c6e1d7a09247efee03d5d8cafe1b7b9d8b1
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-11-26 15:40:43 -0300

    Update 03_02Salute.py

--- Capitulo03_IntroducaoListas/03_02Salute.py
+++ Capitulo03_IntroducaoListas/03_02Salute.py
@@ -1,5 +1,5 @@
 #exercício 3.2 pg73
 names=['Jorge', 'Victor', 'Ângelo']
 print('Olá, ' + names[0].title() + '! Tudo bem?')
-print('Bom dia, ' + names[1].title() + '! Como está a Lú?') 
-print('Fala, ' + names[2].title() + '! Brincando com o Malthus?')
+print('Bom dia, ' + names[1].title() + '! Como está a esposa?') 
+print('Fala, ' + names[2].title() + '! Brincando com o filho?')
