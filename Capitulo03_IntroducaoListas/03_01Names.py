a658451adaf580e24b7feafaca3f85cbd161e49a
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-12-02 13:44:30 -0300

    Update 03_01Names.py

--- Capitulo03_IntroducaoListas/03_01Names.py
+++ Capitulo03_IntroducaoListas/03_01Names.py
@@ -1,6 +1,6 @@
-#3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
+"""3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
 chamada names. Exiba o nome de cada pessoa acessando cada elemento da
-lista, um de cada vez.
+lista, um de cada vez."""
 #pg73
 
 names=['Jorge', 'Victor', 'Ângelo']
