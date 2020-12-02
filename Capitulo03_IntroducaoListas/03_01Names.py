c1e00f37a551f908cf41432413f15968c2b08c2c
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-12-02 13:36:45 -0300

    Update 03_01Names.py

--- Capitulo03_IntroducaoListas/03_01Names.py
+++ Capitulo03_IntroducaoListas/03_01Names.py
@@ -1,4 +1,8 @@
-#exercício 3.1 pg73
+#3.1 – Nomes: Armazene os nomes de alguns de seus amigos em uma lista
+chamada names. Exiba o nome de cada pessoa acessando cada elemento da
+lista, um de cada vez.
+#pg73
+
 names=['Jorge', 'Victor', 'Ângelo']
 print(names[0].title())
 print(names[1].title())
