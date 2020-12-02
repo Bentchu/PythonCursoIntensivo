422cdea575739f5752372ce000536a3e0994a792
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-12-02 13:38:53 -0300

    Update 03_02Salute.py

--- Capitulo03_IntroducaoListas/03_02Salute.py
+++ Capitulo03_IntroducaoListas/03_02Salute.py
@@ -1,4 +1,9 @@
-#exercício 3.2 pg73
+#3.2 – Saudações: Comece com a lista usada no Exercício 3.1, mas em vez de
+simplesmente exibir o nome de cada pessoa, apresente uma mensagem a elas.
+O texto de cada mensagem deve ser o mesmo, porém cada mensagem deve
+estar personalizada com o nome da pessoa.
+#pg73
+
 names=['Jorge', 'Victor', 'Ângelo']
 print('Olá, ' + names[0].title() + '! Tudo bem?')
 print('Bom dia, ' + names[1].title() + '! Como está a esposa?') 
