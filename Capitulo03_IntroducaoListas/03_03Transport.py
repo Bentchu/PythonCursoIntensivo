00452f17eff3ff981bf5cbbafc3f3a4aecb34494
Bentchu <31457200+Bentchu@users.noreply.github.com>
2020-12-02 13:45:29 -0300

    Update 03_03Transport.py

--- Capitulo03_IntroducaoListas/03_03Transport.py
+++ Capitulo03_IntroducaoListas/03_03Transport.py
@@ -1,3 +1,9 @@
+"""3.3 – Sua própria lista: Pense em seu meio de transporte preferido, como
+motocicleta ou carro, e crie uma lista que armazene vários exemplos. Utilize sua
+lista para exibir uma série de frases sobre esses itens, como 'Gostaria de ter
+uma moto Honda'."""
+#pg73
+
 planes=['Grippen', 'Rafale', 'F-35', 'Flanker']
 message = "Gostaria de voar em um " + planes[0].title() + "!"
 print(message)
