e4ef28a255c9fa89716becc8c728479217b18b07
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-01-06 13:52:00 -0300

    Add files via upload

---
+++ Capitulo06_Dicionarios/06_01CadastroNomePessoa.py
@@ -0,0 +1,16 @@
+#6.1 – Pessoa: Use um dicionário para armazenar informações sobre uma 
+#pessoa que você conheça. Armazene seu primeiro nome, o sobrenome, a 
+#idade e a cidade em que ela vive. Você deverá ter chaves como 
+#first_name, last_name,age e city. Mostre cada informação armazenada 
+#em seu dicionário.
+#07/01/21
+
+pessoa = {'primeiro_nome': 'andressa', 
+    'sobrenome': 'benteu',
+    'idade': '42',
+    'cidade': 'tremembé'}
+    
+print("Nome: " + pessoa['primeiro_nome'])
+print("Sobrenome: " + pessoa['sobrenome'])
+print("Idade: " + pessoa['idade'])
+print("Cidade: " + pessoa['cidade'])
