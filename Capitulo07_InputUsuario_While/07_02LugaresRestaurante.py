6bcced9bcccf0ef93f7069a88e50ead231333557
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-07-09 18:55:30 -0300

    Delete 07_02LugaresRestaurante.py

--- Capitulo07_InputUsuario_While/07_02LugaresRestaurante.py
+++
@@ -1,6 +0,0 @@
-'''7.2 – Lugares em um restaurante: Escreva um programa que pergunte ao 
-usuário quantas pessoas estão em seu grupo para jantar. Se a resposta 
-for maior que oito, exiba uma mensagem dizendo que eles deverão esperar 
-uma mesa. Caso contrário, informe que sua mesa está pronta.
-livro pg 171 pdf pg 156
-'''
