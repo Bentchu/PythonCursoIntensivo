9bcfc33012758485256155bf2f8f280d4c24660d
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-07-13 19:40:37 -0300

    Update papagaio3.py

--- Capitulo07_InputUsuario_While/papagaio3.py
+++ Capitulo07_InputUsuario_While/papagaio3.py
@@ -1,9 +1,9 @@
-'''Preimeiro exemplo de flag'''
+'''Primeiro exemplo de flag'''
 
 prompt = "\nMe diga algo, e eu repetirei de volta à você. "
 prompt += "\nEntre 'quit' para encerrar o programa.     "
 
-active =True
+active = True
 while active:
 	mensagem = input(prompt)
 	
