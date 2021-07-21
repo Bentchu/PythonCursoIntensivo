c5b4ac8752382b417b0ca1bccd110f559443a5f7
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-07-21 18:44:48 -0300

    Add files via upload

---
+++ Capitulo07_InputUsuario_While/07_05IngressoCinema.py
@@ -0,0 +1,15 @@
+'''falta descrição'''
+
+idade = "\nQual a idade da pessoa que deseja o ingresso?  "
+
+idade = input(idade)
+idade = int(idade)
+
+if idade < 3:
+	print ("\nO ingresso para menores de 3 anos é gratuito.")
+	
+elif idade < 12:
+	print ("\nO ingresso para clientes de 3 a 12 anos custa 10 dólares.")
+		
+else:
+	print ("\nO ingresso para maiores de 12 anos custa 15 dólares.")
