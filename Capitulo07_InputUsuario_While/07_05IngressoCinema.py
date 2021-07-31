146b9264a1212769496ae157baef4a6b16a7ed0c
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-07-31 08:52:00 -0300

    Update 07_05IngressoCinema.py

--- Capitulo07_InputUsuario_While/07_05IngressoCinema.py
+++ Capitulo07_InputUsuario_While/07_05IngressoCinema.py
@@ -1,4 +1,10 @@
-'''falta descrição'''
+'''7.5 – Ingressos para o cinema: Um cinema cobra preços diferentes para os
+ingressos de acordo com a idade de uma pessoa. Se uma pessoa tiver menos
+de 3 anos de idade, o ingresso será gratuito; se tiver entre 3 e 12 anos, o
+ingresso custará 10 dólares; se tiver mais de 12 anos, o ingresso custará 15
+dólares. Escreva um laço em que você pergunte a idade aos usuários e, então,
+informe-lhes o preço do ingresso do cinema.
+livro pg.179 pdf pg.163'''
 
 idade = "\nQual a idade da pessoa que deseja o ingresso?  "
 
