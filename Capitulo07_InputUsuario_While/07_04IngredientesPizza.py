b5a43be1ba7a6c2e9bb1c233ddc5e2634d32e5ad
Bentchu <31457200+Bentchu@users.noreply.github.com>
2021-07-31 08:50:41 -0300

    Update 07_04IngredientesPizza.py

--- Capitulo07_InputUsuario_While/07_04IngredientesPizza.py
+++ Capitulo07_InputUsuario_While/07_04IngredientesPizza.py
@@ -1,4 +1,8 @@
-'''falta descrição'''
+'''7.4 – Ingredientes para uma pizza: Escreva um laço que peça ao usuário para
+fornecer uma série de ingredientes para uma pizza até que o valor 'quit' seja
+fornecido. À medida que cada ingrediente é especificado, apresente uma
+mensagem informando que você acrescentará esse ingrediente à pizza.
+livro pg.178 pdf pg.163'''
 
 prompt = "\nQual ingrediente deseja em sua pizza?. "
 prompt += "\nEntre 'quit' para encerrar o programa.     "
